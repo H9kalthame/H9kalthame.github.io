3cf768f7626b9263a81cc55611527dd6e6b7f217
Hisham Dhafer <107688234+H9kalthame@users.noreply.github.com>
2023-08-28 14:12:04 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
     $(window).scroll(function () {
-        // checks if window is scrolled more than 500px, adds/removes solid class
         if ($(this).scrollTop() > 550) {
             $('.navbar').addClass('solid');
             $('.back-to-top').addClass('visible');
@@ -8,32 +7,26 @@ $(document).ready(function () {
             $('.navbar').removeClass('solid');
             $('.back-to-top').removeClass('visible');
         }
-
     });
-});
-
-
-$(document).ready(function () {
-    // Add smooth scrolling to all links
+    function smoothScroll(target) {
+        const targetElement = document.querySelector(target);
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    }
     $("a").on('click', function (event) {
-
-        // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
-            // Prevent default anchor click behavior
             event.preventDefault();
-
-            // Store hash
             var hash = this.hash;
 
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function () {
+            if ($(hash).length > 0) {
+                smoothScroll(hash);
 
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-            });
-        } // End if
+                history.pushState(null, null, hash);
+            }
+        }
     });
 });
